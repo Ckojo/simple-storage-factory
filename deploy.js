34cf096a20e1607707f238a7bcafb5e9fe653efe
Jovan <jovansr@protonmail.com>
2022-06-11 12:52:29 -0400

    Remove part with own transaction data - to see this -> contract-with-own-transaction-data branch

--- deploy.js
+++ deploy.js
@@ -16,22 +16,6 @@ async function main() {
   const contract = await contractFactory.deploy(); // STOP here, wait for contract to deploy
   await contract.deployTransaction.wait(1); // THIS IS A KIND OF TRANSACTION RECEIPT
 
-  // ============================== THIS IS HOW WE CAN DEPLOY WITH OUR OWN TRANSACTION DATA ==============================
-  // console.log("Deploying with only transaction data");
-  // const nonce = await wallet.getTransactionCount();
-  // const tx = {
-  //   nonce: nonce,
-  //   gasPrice: 20000000000,
-  //   gasLimit: 6721975,
-  //   to: null,
-  //   value: 0,
-  //   data: "0x608",
-  //   chainId: 1337,
-  // };
-  // const sentTxResponse = await wallet.sendTransaction(tx);
-  // await sentTxResponse.wait(1);
-  // console.log(sentTxResponse);
-
   const currentFavoriteNumber = await contract.retrieve();
   console.log(`Current favorite number: ${currentFavoriteNumber.toString()}`);
 
